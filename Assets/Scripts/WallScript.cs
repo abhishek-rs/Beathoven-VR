cf048cf237b2f45599c2051f9825248ab4a1ec07
Nevalainen Susanna <nevalas5@niksula.hut.fi>
2017-04-17 12:21:08 +0300

    custom materials created to resources file, color changing code draft added to walls

--- Assets/Scripts/WallScript.cs
+++ Assets/Scripts/WallScript.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class WallScript : MonoBehaviour {
 
 	public float smoothTime = 0.5F;
-    public float yVelocity = 0.0F;
-    
+	public float yVelocity = 0.0F;
+
 
 	// Use this for initialization
 	void Start () {
-		
+
 		int currentSong = ApplicationModel.currentSong;
 
 		if(ApplicationModel.songs[currentSong, 2] > 0.70)
 			this.GetComponent<Renderer>().material.color = Color.black;
 		else
 			this.GetComponent<Renderer>().material.color = new Color( 0.529f, 0.807f, 0.980f, 1.0f);
-		}
-	
+	}
+
+	/*	not tested yet, might work
+		if(ApplicationModel.songs[currentSong, 2] > 0.70){
+			// Assigns a material named "Assets/Resources/stormySky" to the object.
+			Material stormySky = Resources.Load("stormySky", typeof(Material)) as Material;
+			this.GetComponent.renderer.material = stormySky;
+		}else{
+		Material blueSky = Resources.Load("blueSky", typeof(Material)) as Material;
+			this.GetComponent.renderer.material = blueSky;
+		}*/
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
+
