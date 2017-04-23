0f2f7483241216bb6ee010587c2cf0638b871523
Alessandro Capra <alessandro.cpr@gmail.com>
2017-04-24 00:41:39 +0300

    Mapped 512 frequencies in 8

--- Assets/Scripts/AudioSpeaker.cs
+++ Assets/Scripts/AudioSpeaker.cs
@@ -8,6 +8,7 @@ public class AudioSpeaker : MonoBehaviour {
 	//Object[] myMusic;
 	AudioSource _audioSource;
 	public static float[] _samples = new float[512]; 
+	public static float[] _frequencyBand = new float[8];
 
  	//void Awake(){
     //    myMusic = Resources.LoadAll("Music",typeof(AudioClip));
@@ -24,10 +25,36 @@ public class AudioSpeaker : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		getSpectrumAudioSource ();
+		makeFrequBands ();
 		
 	}
 
 	void getSpectrumAudioSource(){
 		_audioSource.GetSpectrumData (_samples, 0, FFTWindow.Blackman);
+	}
+
+	void makeFrequBands(){
+
+		int count = 0;
+
+		for (int i = 0; i < 8; i++) {
+
+			float average = 0;
+			int sampCount = (int)Mathf.Pow (2, i) * 2;
+
+			if (i == 7) {
+				sampCount += 2;
+			}
+
+			for (int j = 0; j < sampCount; j++) {
+				average += _samples [count] * (count + 1);
+				count++;
+			}
+
+			average /= count;
+
+			_frequencyBand [i] = average * 10;
+		}
+	
 	}
 }
