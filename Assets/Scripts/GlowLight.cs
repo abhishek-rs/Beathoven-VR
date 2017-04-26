503b9d64753b5e09fdada55cb5acae006c010f47
Alessandro Capra <alessandro.cpr@gmail.com>
2017-04-26 23:42:13 +0300

    Experiments with lightning

---
+++ Assets/Scripts/GlowLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowLight : MonoBehaviour {
+
+	public float duration = 1.0F;
+	public Color color0 = Color.red;
+	public Color color1 = Color.blue;
+	public Light lt;
+
+	void Start() {
+		lt = GetComponent<Light>();
+	}
+
+	void Update() {
+		float t = Mathf.PingPong(Time.time, duration) / duration;
+		lt.color = Color.Lerp(color0, color1, t);
+	}
+}
