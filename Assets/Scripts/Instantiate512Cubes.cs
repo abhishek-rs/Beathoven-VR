d96e63c3859c719e0a7513c84de6b59070133dc8
Alessandro Capra <alessandro.cpr@gmail.com>
2017-04-28 20:02:26 +0300

    Added idea on how to change color of cubes

--- Assets/Scripts/Instantiate512Cubes.cs
+++ Assets/Scripts/Instantiate512Cubes.cs
@@ -7,6 +7,7 @@ public class Instantiate512Cubes : MonoBehaviour {
 	public GameObject _sampleCubePrefab;
 	GameObject[] _sampleCube = new GameObject[512];
 	public float _maxScale;
+	int currentSong = ApplicationModel.currentSong;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +20,18 @@ public class Instantiate512Cubes : MonoBehaviour {
 			this.transform.eulerAngles = new Vector3 (0, -5.625f * i, 0);
 			_instanceSampleCube.transform.position = Vector3.forward * 100;
 			_sampleCube [i] = _instanceSampleCube;
+
+			// change color based on Spotify data (to fix, renderer not working)
+
+			//if (ApplicationModel.songs [this.currentSong, 2] > 0.70) {
+			//	_sampleCube[i].GetComponent<Renderer> ().material.color = Color.red;
+			//} else if (ApplicationModel.songs [this.currentSong, 2] <= 0.70) {
+			//	if (ApplicationModel.songs [this.currentSong, 1] > 0.50) {
+			//		_sampleCube[i].GetComponent<Renderer> ().material.color = Color.cyan;	
+			//	} else {
+			//		_sampleCube[i].GetComponent<Renderer> ().material.color = Color.gray;	
+			//	}
+			//}
 		}
 	}
 
