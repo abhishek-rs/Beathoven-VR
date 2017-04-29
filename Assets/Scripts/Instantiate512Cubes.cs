87fe03e2f6e32bd6c8cd1cbe0f0ae4d5de0a756c
Alessandro Capra <alessandro.cpr@gmail.com>
2017-04-29 15:04:46 +0300

    Fixed lights, cubes changing color

--- Assets/Scripts/Instantiate512Cubes.cs
+++ Assets/Scripts/Instantiate512Cubes.cs
@@ -17,28 +17,33 @@ public class Instantiate512Cubes : MonoBehaviour {
 			_instanceSampleCube.transform.parent = this.transform;
 			_instanceSampleCube.name = "SampleCube " + i;
 
+			MeshRenderer m =_instanceSampleCube.AddComponent<MeshRenderer>() as MeshRenderer;
+			m.enabled = true;
+
 			this.transform.eulerAngles = new Vector3 (0, -5.625f * i, 0);
 			_instanceSampleCube.transform.position = Vector3.forward * 100;
 			_sampleCube [i] = _instanceSampleCube;
 
 			// change color based on Spotify data (to fix, renderer not working)
 
-			//if (ApplicationModel.songs [this.currentSong, 2] > 0.70) {
-			//	_sampleCube[i].GetComponent<Renderer> ().material.color = Color.red;
-			//} else if (ApplicationModel.songs [this.currentSong, 2] <= 0.70) {
-			//	if (ApplicationModel.songs [this.currentSong, 1] > 0.50) {
-			//		_sampleCube[i].GetComponent<Renderer> ().material.color = Color.cyan;	
-			//	} else {
-			//		_sampleCube[i].GetComponent<Renderer> ().material.color = Color.gray;	
-			//	}
-			//}
+			_sampleCube[i].GetComponent<Renderer> ().material.color = Color.Lerp(Color.white, Color.blue, Mathf.PingPong(Time.time, 1));
+
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
 		for (int i = 0; i < 64; i++) {
+
 			if (_sampleCube != null) {
+
+				if(ApplicationModel.songs[ApplicationModel.currentSong, 0] == 1.00){
+					_sampleCube[i].transform.GetChild(0).GetComponent<Renderer>().material.color = Color.Lerp( new Color( 1.0f, 0.549f, 0.0f, 1.0f), Color.yellow, Mathf.PingPong(Time.time, 1));
+				}
+				else{
+					_sampleCube[i].transform.GetChild(0).GetComponent<Renderer>().material.color = Color.Lerp(Color.white, Color.blue, Mathf.PingPong(Time.time, 1));
+				}
+
 				_sampleCube [i].transform.localScale = new Vector3 (10, (AudioSpeaker._samples [i] * _maxScale) + 2, 10);
 			}
 		}
