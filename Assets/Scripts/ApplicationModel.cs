666b6bd2499909e7ee20e29a6440687f6045c8cc
Alessandro Capra <alessandro.cpr@gmail.com>
2017-04-29 15:25:22 +0300

    Fixed spotify values, camera, cubes colors

--- Assets/Scripts/ApplicationModel.cs
+++ Assets/Scripts/ApplicationModel.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ApplicationModel {
 
-	static public int currentSong = 1;
+	static public int currentSong = 0;
 	static public double [,] songs = new double [4,5]{ 
 								//mode, valence, energy, positivity, negativity 
-    							{1.00, 0.529, 0.456, 0.068, 0.115}, 			// Sound of silence 
-    							{1.00, 0.390, 0.540, 0.347, 0.089}, 			// Beatles - Here comes the sun  
+    							{1.00, 0.429, 0.433, 0.049, 0.024}, 			// Bowie - Space Oddity 
+    							{0.00, 0.179, 0.376, 0.172, 0.095}, 			// Beatles - Come together  
     							{1.00, 0.080, 0.976, 0.132, 0.018},				// ACDC - Let there be rock
     							{0.00, 0.232, 0.404, 0.108, 0.093},				// Queen - Bohemian rhapsody 
     							};
